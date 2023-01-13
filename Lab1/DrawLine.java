335b4355bf0f62a520245c801ecbb7e046699ea9
Vish.Siri <s.vish555@gmail.com>
2023-01-13 15:51:56 +0700

    DrawLine

---
+++ Lab1/DrawLine.java
@@ -0,0 +1,31 @@
+//Vish Siriwatana 64050229
+
+
+package Lab1;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.*;
+import java.util.Random;
+
+public class DrawLine extends JPanel {
+    final Random r = new Random();
+    int dotAmount = 100;
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        DrawLine m = new DrawLine();
+        f.add(m);
+        f.setTitle("Vish Siriwatana 64050229");
+        f.setSize(600, 600);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        int width = this.getWidth();
+        int height = this.getHeight();
+        g.setColor(Color.BLACK);
+        g.drawLine(0, 0, width, height);
+    }
+
+}
